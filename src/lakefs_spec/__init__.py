a798ac749c030562257d1dcec861247bc0004484
Adrian Rumpold <a.rumpold@gmail.com>
2023-08-25 10:00:48 +0200

    feat: Include setuptools-scm for version derivation from Git
    This commit adds setuptools-scm as a build dependency in order to
    dynamically determine the version number of the package from Git tags.
    
    This prevents inconsistencies between the versions in the package and
    the repository, which have already bitten us before.
    
    Issue: #25

--- src/lakefs_spec/__init__.py
+++ src/lakefs_spec/__init__.py
@@ -1,3 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .spec import LakeFSFile, LakeFSFileSystem
 
-version = "0.1.1"
+try:
+    __version__ = version("lakefs-spec")
+except PackageNotFoundError:
+    # package is not installed
+    pass
