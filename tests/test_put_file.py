dc3cfb80c81438c1dacc1d18bdf151a82fa3a419
Max Mynter <maxmynter@me.com>
2023-08-29 15:59:58 +0200

    test(Implicit Branch Creation):

--- tests/test_put_file.py
+++ tests/test_put_file.py
@@ -1,5 +1,9 @@
 from lakefs_spec import LakeFSFileSystem
 from tests.util import RandomFileFactory
+from lakefs_client.exceptions import NotFoundException
+import pytest 
+import random
+import string
 
 
 def test_put_with_default_commit_hook(
@@ -56,3 +60,61 @@ def test_no_change_postcommit(
     )
     assert commits.results[0] == latest_commit
     # in particular, this test asserts that no API exception happens in postcommit.
+
+def test_implicit_branch_creation(
+    random_file_factory: RandomFileFactory,
+    fs: LakeFSFileSystem,
+    repository: str,
+    temp_branch: str):
+
+    # Create Changes to commit. 
+    random_file = random_file_factory.make()
+    lpath = str(random_file)
+
+
+    with fs.scope(create_branch_ok = True):
+        #Pushing to an existing branch
+        rpath = f"{repository}/{temp_branch}/{random_file.name}"
+        fs.put(lpath, rpath)
+        commits = fs.client.commits_api.log_branch_commits(
+            repository=repository,
+            branch=temp_branch,
+        )
+        latest_commit = commits.results[0]  # commit log is ordered branch-tip-first
+        assert latest_commit.message == f"Add file {random_file.name}"
+
+        #Creates non-existing branch and then pushes to it.
+        non_existing_branch = 'non-existing-' + "".join(random.choices(string.digits, k=8))
+        rpath = f"{repository}/{non_existing_branch}/{random_file.name}"
+        fs.put(lpath, rpath)
+        commits = fs.client.commits_api.log_branch_commits(
+            repository=repository,
+            branch=non_existing_branch,
+        )
+        latest_commit = commits.results[0]  # commit log is ordered branch-tip-first
+        assert latest_commit.message == f"Add file {random_file.name}"
+        assert len(commits.results) == 2  # 2 commits: Repository Created and File Added.   
+        #Created a new branch with new commit 
+        
+        #Clean the test state and delete the implicitly created branch.
+        fs.client.branches_api.delete_branch( 
+                repository=repository,
+                branch=non_existing_branch,
+            )
+
+    with fs.scope(create_branch_ok = False):
+        #Pushing to an existing Branch
+        rpath = f"{repository}/{temp_branch}/{random_file.name}"
+        fs.put(lpath, rpath)
+        commits = fs.client.commits_api.log_branch_commits(
+            repository=repository,
+            branch=temp_branch,
+        )
+        latest_commit = commits.results[0]  # commit log is ordered branch-tip-first
+        assert latest_commit.message == f"Add file {random_file.name}"
+
+        #Throws Error for non-existing branch with create_branch_ok = False
+        another_non_existing_branch = 'non-existing-' + "".join(random.choices(string.digits, k=8))
+        rpath = f"{repository}/{another_non_existing_branch}/{random_file.name}"
+        with pytest.raises(NotFoundException):
+            fs.put(lpath, rpath) 
