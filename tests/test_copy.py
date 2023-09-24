ffc9758720bc76cb866dba89f18235faf40ac00b
Nicholas Junge <nicholas.junge@web.de>
2023-09-24 19:50:20 +0200

    Add tests for `fs.cp_file` edge cases
    1) no-op on `path1 == path2`.
    2) `ValueError` on a copy attempt into a different repo.

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -1,7 +1,9 @@
 from typing import Any
 
+import pytest
+
 from lakefs_spec import LakeFSFileSystem
-from tests.util import RandomFileFactory
+from tests.util import RandomFileFactory, with_counter
 
 
 def test_copy(
@@ -23,3 +25,20 @@ def test_copy(
         new_rpath = f"{repository}/{b}/{random_file.name}"
         fs.cp_file(rpath, new_rpath)
         assert fs.exists(new_rpath)
+
+
+def test_copy_edge_cases(
+    fs: LakeFSFileSystem,
+    repository: str,
+    temp_branch: str,
+) -> None:
+    fs.client, counter = with_counter(fs.client)
+
+    path = f"{repository}/main/lakes.parquet"
+
+    fs.cp_file(path, path)
+
+    assert counter.count("objects_api.copy_object") == 0
+
+    with pytest.raises(ValueError, match="can only copy objects within a repository.*"):
+        fs.cp_file(path, "my-repo/main/lakes.parquet")
