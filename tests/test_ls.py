365ad94ed5822c9f1cdb914b659c316c0c9ad82b
Nicholas Junge <nicholas.junge@web.de>
2023-09-24 16:17:58 +0200

    Add a listings cache test
    Verifies that the cache is hit on two identical `fs.ls` calls.
    
    Does not assume cache size control or TTL, so this only checks a cache
    hit in the default configuration.

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -1,4 +1,5 @@
 from lakefs_spec import LakeFSFileSystem
+from tests.util import with_counter
 
 
 def test_paginated_ls(fs: LakeFSFileSystem, repository: str) -> None:
@@ -13,3 +14,21 @@ def test_paginated_ls(fs: LakeFSFileSystem, repository: str) -> None:
     for pagesize in [2, 5, 10, 50]:
         paged_results = fs.ls(resource, amount=pagesize)
         assert paged_results == all_results
+
+
+def test_ls_caching(fs: LakeFSFileSystem, repository: str) -> None:
+    """
+    Check that ls calls are properly cached.
+    """
+    fs.client, counter = with_counter(fs.client)
+
+    testdir = "data"
+    resource = f"{repository}/main/{testdir}/"
+
+    for _ in range(2):
+        fs.ls(resource)
+        assert len(fs.dircache) == 1
+        assert tuple(fs.dircache.keys()) == (testdir,)
+
+    # assert the second `ls` call hits the cache
+    assert counter.count("objects_api.list_objects") == 1
