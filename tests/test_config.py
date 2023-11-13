c0a4cd0a85294aac10a22e51d1b8b0f6bf3e4789
Adrian Rumpold <a.rumpold@gmail.com>
2023-11-13 14:35:10 +0100

    chore: Remove unnecessary code and test
    Since the PyYAML dependency is now mandatory, the
    check is no longer necessary (as is the corresponding test).
    
    Issue: #141

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,4 @@
-import sys
-from pathlib import Path
-
 import pytest
-from pytest import MonkeyPatch
 
 from lakefs_spec.config import LakectlConfig
 
@@ -14,13 +10,3 @@ def test_config_read_nonexistent_file():
     """
     with pytest.raises(FileNotFoundError):
         LakectlConfig.read("aaaaaaaaaaaaaaaa.yaml")
-
-
-def test_lakectl_config_parsing_without_yaml(
-    monkeypatch: MonkeyPatch, temporary_lakectl_config: str
-) -> None:
-    # unset YAML module from sys.modules.
-    monkeypatch.setitem(sys.modules, "yaml", None)
-
-    with pytest.warns(UserWarning, match="`pyyaml` is not installed"):
-        LakectlConfig.read(Path("~/.lakectl.yaml").expanduser())
