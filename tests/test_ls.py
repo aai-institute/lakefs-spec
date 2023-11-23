543ddf4e55b4a1935ae6179d723afee76b000542
Adrian Rumpold <a.rumpold@gmail.com>
2023-11-23 16:20:15 +0100

    Improvements to `ls` implementation (#166)
    * fix: Make `ls` and dircache use fully-qualified paths
    
    Issues: #162, #164
    
    * feat: Add `refresh` kwarg support to `ls`
    
    This flag can be used to explicitly bypass the dircache.
    It is suggested in the docstring of the base method,
    so our implementation now follows the intended semantics.
    
    Issue: #163

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -28,10 +28,28 @@ def test_ls_caching(fs: LakeFSFileSystem, repository: str) -> None:
     for _ in range(2):
         fs.ls(resource)
         assert len(fs.dircache) == 1
-        assert tuple(fs.dircache.keys()) == (testdir,)
+        assert set(fs.dircache.keys()) == {resource.removesuffix("/")}
 
     # assert the second `ls` call hits the cache
     assert counter.count("objects_api.list_objects") == 1
+
+
+def test_ls_cache_refresh(fs: LakeFSFileSystem, repository: str) -> None:
+    """
+    Check that ls calls bypass the dircache if requested through ``refresh=False``
+    """
+    fs.client, counter = with_counter(fs.client)
+
+    testdir = "data"
+    resource = f"{repository}/main/{testdir}/"
+
+    for _ in range(2):
+        fs.ls(resource, refresh=True)
+        assert len(fs.dircache) == 1
+        assert set(fs.dircache.keys()) == {resource.removesuffix("/")}
+
+    # assert the second `ls` call bypasses the cache
+    assert counter.count("objects_api.list_objects") == 2
 
 
 def test_ls_stale_cache_entry(
@@ -46,11 +64,13 @@ def test_ls_stale_cache_entry(
 
     resource = f"{repository}/main/data/"
 
-    fs.ls(resource)
+    res = fs.ls(resource)
     assert counter.count("objects_api.list_objects") == 1
-    assert tuple(fs.dircache.keys()) == ("data",)
+    assert set(fs.dircache.keys()) == {resource.removesuffix("/")}
 
-    cache_entry = fs.dircache["data"]
+    cache_entry = fs.dircache[resource.removesuffix("/")]
+    assert len(cache_entry) == 1
+    assert cache_entry[0] == res[0]
 
     lpath = str(random_file)
     rpath = f"{repository}/{temp_branch}/data/{random_file.name}"
@@ -59,26 +79,34 @@ def test_ls_stale_cache_entry(
 
     res = fs.ls(rpath)
     assert counter.count("objects_api.list_objects") == 2
-    # is the file now added to the cache entry?
-    assert res[0] in cache_entry
+
+    # Should not be added to the cache for the other branch...
+    assert len(cache_entry) == 1
+
+    # ... but instead to the cache entry for the new branch
+    cache_entry = fs.dircache[f"{repository}/{temp_branch}/data"]
+    assert len(cache_entry) == 1
+    assert cache_entry[0] == res[0]
 
 
 def test_ls_no_detail(fs: LakeFSFileSystem, repository: str) -> None:
     fs.client, counter = with_counter(fs.client)
 
-    resource = f"{repository}/main/data"
+    branch = "main"
+    prefix = f"{repository}/{branch}"
+    resource = f"{prefix}/data"
 
-    expected = ["data/lakes.source.md"]
+    expected = [f"{resource}/lakes.source.md"]
     # first, verify the API fetch does the expected...
     assert fs.ls(resource, detail=False) == expected
-    assert list(fs.dircache.keys()) == ["data"]
+    assert list(fs.dircache.keys()) == [resource]
 
     # ...as well as the cache fetch.
     assert fs.ls(resource, detail=False) == expected
     assert counter.count("objects_api.list_objects") == 1
 
     # test the same thing with a subfolder + file prefix
-    resource = f"{repository}/main/images/duckdb"
+    resource = f"{prefix}/images/duckdb"
     fs.ls(resource, detail=False)
 
-    assert list(fs.dircache.keys()) == ["data", "images"]
+    assert set(fs.dircache.keys()) == {f"{prefix}/data", f"{prefix}/images"}
