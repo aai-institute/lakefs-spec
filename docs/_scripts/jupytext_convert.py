6906b68aba9fb58da7ed9f4afb2e4ba53e41d400
Adrian Rumpold <a.rumpold@gmail.com>
2023-11-30 07:40:44 +0100

    docs: Simplify Jupytext->ipynb generator script

--- docs/_scripts/jupytext_convert.py
+++ docs/_scripts/jupytext_convert.py
@@ -6,8 +6,6 @@ from pathlib import Path
 import jupytext
 import mkdocs_gen_files
 
-nav = mkdocs_gen_files.Nav()
-
 for path in sorted(Path("docs/tutorials").rglob("*.py")):
     ipynb_path = path.relative_to(Path("docs")).with_suffix(".ipynb")
     logging.debug(f"Converting {path} -> {ipynb_path}")
@@ -18,7 +16,5 @@ for path in sorted(Path("docs/tutorials").rglob("*.py")):
     with mkdocs_gen_files.open(ipynb_path, "wb") as fd:
         content = jupytext.writes(jupytext_nb, fmt="notebook")
         fd.write(content.encode("utf8"))
-
-    nav[path.parts] = ipynb_path.as_posix()
 
     mkdocs_gen_files.set_edit_path(ipynb_path, path)
