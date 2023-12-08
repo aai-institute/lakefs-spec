3ac47487326928f75decd4b09a9932cd1d581b15
Nicholas Junge <nicholas.junge@web.de>
2023-12-08 14:39:46 +0100

    Add `readline(s)` and `touch` tests
    Needed to complete the file functionality tests. Implementations are
    checked against builtin `open(..., "rb")`.

---
+++ tests/smoke_tests/test_abstractfile.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from lakefs_spec.spec import LakeFSFileSystem
+
+
+def test_readline(
+    fs: LakeFSFileSystem,
+    repository: str,
+    temp_branch: str,
+) -> None:
+    """Checks that `fs.open()` behaves like builtin `open` for `readline(s)` APIs."""
+    lpath = Path("random_file.txt")
+    try:
+        lpath.write_text("Hello\nmy name is\nxyz")
+        with open(lpath, "rb") as f:
+            native_open_line = f.readline()
+            f.seek(0)
+            native_open_lines = f.readlines()
+
+        rpath = f"{repository}/{temp_branch}/{Path(lpath).name}"
+        fs.put_file(lpath, rpath)
+
+        with fs.open(rpath, "rb") as rf:
+            # mode == "rb" means everything is bytes.
+            assert rf.readline() == native_open_line
+            rf.seek(0)
+            assert rf.readlines() == native_open_lines
+    finally:
+        lpath.unlink(missing_ok=True)
+
+
+def test_touch(
+    fs: LakeFSFileSystem,
+    repository: str,
+    temp_branch: str,
+) -> None:
+    rpath = f"{repository}/{temp_branch}/hello.txt"
+    fs.touch(rpath)
+    assert fs.exists(rpath)
