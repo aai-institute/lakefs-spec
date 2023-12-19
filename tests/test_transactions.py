77a9e2e333bb5128f8db64279ad6306cc1793b5e
Nicholas Junge <nicholas.junge@web.de>
2023-12-19 16:46:40 +0100

    Migrate lakeFS transaction to SDK wrapper
    Inlines all of the resource-related operations since they replace the
    client helpers. Thanks to the new builder patterns, the calls save
    significant space compared to the client helpers.
    
    Allow `Repository` for all repository arguments, and `Reference` objects
    for most ref-ish arguments (branches, tags, commits).
    
    Since `Commit` is unfortunately not a descendant of `Reference`, we need
    to include `Commits` separately.

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,6 +1,8 @@
 from typing import Any
 
-from lakefs_sdk.models import Commit
+from lakefs.branch import Branch
+from lakefs.reference import Reference
+from lakefs.repository import Repository
 
 from lakefs_spec import LakeFSFileSystem
 from tests.util import RandomFileFactory, with_counter
@@ -9,13 +11,13 @@ from tests.util import RandomFileFactory, with_counter
 def test_transaction_commit(
     random_file_factory: RandomFileFactory,
     fs: LakeFSFileSystem,
-    repository: str,
-    temp_branch: str,
+    repository: Repository,
+    temp_branch: Branch,
 ) -> None:
     random_file = random_file_factory.make()
 
     lpath = str(random_file)
-    rpath = f"{repository}/{temp_branch}/{random_file.name}"
+    rpath = f"{repository.id}/{temp_branch.id}/{random_file.name}"
 
     message = f"Add file {random_file.name}"
 
@@ -30,112 +32,94 @@ def test_transaction_commit(
 
     assert sha.available
 
-    commits = fs.client.refs_api.log_commits(
-        repository=repository,
-        ref=temp_branch,
-    )
-    latest_commit = commits.results[0]
-
+    commits = list(temp_branch.log())
+    assert len(commits) > 0
+    latest_commit = commits[0]
     assert latest_commit.message == message
     assert latest_commit.id == sha.id
 
 
-def test_transaction_tag(fs: LakeFSFileSystem, repository: str) -> None:
+def test_transaction_tag(fs: LakeFSFileSystem, repository: Repository) -> None:
     try:
         # tag gets created on exit of the context.
         with fs.transaction as tx:
             sha = tx.rev_parse(repository, "main")
-            tag = tx.tag(repository=repository, ref=sha, tag="v2")
+            tag = tx.tag(repository, ref=sha, tag="v2")
 
         assert sha.available
 
-        tags = fs.client.tags_api.list_tags(repository).results
+        tags = list(repository.tags())
+        assert len(tags) > 0
         assert tags[0].id == tag
-        assert tags[0].commit_id == sha.id
+        assert tags[0].get_commit().id == sha.id
     finally:
-        fs.client.tags_api.delete_tag(repository=repository, tag=tag)
+        repository.tag(tag).delete()
 
 
 def test_transaction_merge(
     random_file_factory: RandomFileFactory,
     fs: LakeFSFileSystem,
-    repository: str,
-    temp_branch: str,
+    repository: Repository,
+    temp_branch: Branch,
     temporary_branch_context: Any,
 ) -> None:
     random_file = random_file_factory.make()
 
     with temporary_branch_context("transaction-merge-test") as new_branch:
-        resource = f"{repository}/{new_branch}/{random_file.name}"
+        resource = f"{repository.id}/{new_branch.id}/{random_file.name}"
         message = "Commit new file"
 
         with fs.transaction as tx:
             # stage a file on new_branch...
             fs.put_file(str(random_file), resource)
             # ... commit it with the above message
-            tx.commit(
-                repository=repository,
-                branch=new_branch,
-                message=message,
-            )
+            tx.commit(repository, new_branch, message)
             # ... and merge it into temp_branch.
-            tx.merge(repository=repository, source_ref=new_branch, into=temp_branch)
+            tx.merge(repository, new_branch, into=temp_branch)
 
         # at last, verify temp_branch@HEAD is the merge commit.
-        commits = fs.client.refs_api.log_commits(
-            repository=repository,
-            ref=temp_branch,
-        )
-        latest_commit = commits.results[0]
-        assert latest_commit.message == f"Merge {new_branch!r} into {temp_branch!r}"
-        second_latest_commit = commits.results[1]
+        commits = list(temp_branch.log())
+        assert len(commits) > 2
+        latest_commit = commits[0]
+        assert latest_commit.message == f"Merge {new_branch.id!r} into {temp_branch.id!r}"
+        second_latest_commit = commits[1]
         assert second_latest_commit.message == message
 
 
 def test_transaction_revert(
     random_file_factory: RandomFileFactory,
     fs: LakeFSFileSystem,
-    repository: str,
-    temp_branch: str,
+    repository: Repository,
+    temp_branch: Branch,
 ) -> None:
     random_file = random_file_factory.make()
 
     lpath = str(random_file)
-    rpath = f"{repository}/{temp_branch}/{random_file.name}"
+    rpath = f"{repository.id}/{temp_branch.id}/{random_file.name}"
 
     message = f"Add file {random_file.name}"
 
     with fs.transaction as tx:
         fs.put_file(lpath, rpath, autocommit=False)
         tx.commit(repository, temp_branch, message=message)
-        tx.revert(repository=repository, branch=temp_branch)
+        tx.revert(repository, temp_branch)
 
-    commits = fs.client.refs_api.log_commits(
-        repository=repository,
-        ref=temp_branch,
-    )
-    latest_commit = commits.results[0]
-    assert latest_commit.message == f"Revert {temp_branch}"
+    commits = list(temp_branch.log())
+    assert len(commits) > 1
+    latest_commit = commits[0]
+    assert latest_commit.message == f"Revert {temp_branch.id}"
 
 
-def test_transaction_branch(fs: LakeFSFileSystem, repository: str) -> None:
+def test_transaction_branch(fs: LakeFSFileSystem, repository: Repository) -> None:
     branch = "new-hello"
 
     try:
         with fs.transaction as tx:
-            tx.create_branch(repository=repository, name=branch, source_branch="main")
+            tx.create_branch(repository, branch, source="main")
 
-        branches = [
-            b.id for b in fs.client.branches_api.list_branches(repository=repository).results
-        ]
-
-        # existence check for a newly created branch.
-        assert branch in branches
+        assert branch in [b.id for b in list(repository.branches())]
     finally:
-        fs.client.branches_api.delete_branch(
-            repository=repository,
-            branch=branch,
-        )
+        repository.branch(branch).delete()
 
 
 def test_transaction_entry(fs: LakeFSFileSystem) -> None:
@@ -147,17 +131,17 @@ def test_transaction_entry(fs: LakeFSFileSystem) -> None:
 def test_transaction_failure(
     random_file_factory: RandomFileFactory,
     fs: LakeFSFileSystem,
-    repository: str,
-    temp_branch: str,
+    repository: Repository,
+    temp_branch: Branch,
 ) -> None:
     random_file = random_file_factory.make()
 
     lpath = str(random_file)
-    rpath = f"{repository}/{temp_branch}/{random_file.name}"
+    rpath = f"{repository.id}/{temp_branch.id}/{random_file.name}"
 
     message = f"Add file {random_file.name}"
 
-    fs.client, counter = with_counter(fs.client)
+    fs.client._client, counter = with_counter(fs.client._client)
     try:
         with fs.transaction as tx:
             fs.put_file(lpath, rpath)
@@ -173,22 +157,23 @@ def test_transaction_failure(
 def test_placeholder_representations(
     random_file_factory: RandomFileFactory,
     fs: LakeFSFileSystem,
-    repository: str,
-    temp_branch: str,
+    repository: Repository,
+    temp_branch: Branch,
 ) -> None:
     random_file = random_file_factory.make()
 
     lpath = str(random_file)
-    rpath = f"{repository}/{temp_branch}/{random_file.name}"
+    rpath = f"{repository.id}/{temp_branch.id}/{random_file.name}"
 
     message = f"Add file {random_file.name}"
 
     with fs.transaction as tx:
         fs.put_file(lpath, rpath)
         sha = tx.commit(repository, temp_branch, message=message)
-    latest_commit = fs.client.refs_api.log_commits(repository=repository, ref=temp_branch).results[
-        0
-    ]
-    assert isinstance(sha, Commit)
+
+    assert isinstance(sha, Reference)
+    commits = list(temp_branch.log())
+    assert len(commits) > 0
+    latest_commit = commits[0]
     assert sha.id == latest_commit.id
     assert repr(sha.id) == repr(latest_commit.id)
